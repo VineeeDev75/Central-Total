e8bac65e9caab5107feb580aad72feae7e8be63d
VineeeDev75 <viniciusdev75@gmail.com>
2025-04-27 08:58:41 -0300

    Criação e início de configurações do projeto.

---
+++ backend/centraltotal/src/main/java/br/com/vsm/centraltotal/CentraltotalApplication.java
@@ -0,0 +1,13 @@
+package br.com.vsm.centraltotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentraltotalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentraltotalApplication.class, args);
+	}
+
+}
---
+++ backend/centraltotal/src/test/java/br/com/vsm/centraltotal/CentraltotalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vsm.centraltotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentraltotalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
